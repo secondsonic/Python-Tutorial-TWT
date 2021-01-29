08d359053adcd9800bc7fb36a76e982a5ff4f74f
Arsalan <Arsalanbardsiri@gmail.com>
2021-01-29 14:05:49 -0800

    Tutorial #1

--- Tutorial1.py
+++ Tutorial1.py
@@ -6,5 +6,23 @@ x = 0
 name = 'Arsalan'
 print(x)
 print(name)
+
 name = "Traveler"
 print(name)
+
+age = 21
+print(age)
+
+#restrictions
+
+var = 0
+_var = 1
+var_ = 2
+Varone = 3
+varOne = 4
+VAR = 5
+#invalid 6var = 6
+var6 = 6
+var_7 = 7
+var_eight = 8
+print(var, VAR)
