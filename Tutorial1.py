c9aa43ac711dd9a1397b00b7f40f5caa7ddd12cb
Arsalan <Arsalanbardsiri@gmail.com>
2021-01-29 13:57:47 -0800

    Tutorial #1

---
+++ Tutorial1.py
@@ -0,0 +1,10 @@
+#int 1, 2, 76, -5, 0
+#str "Arsalan" , 'Bardsiri' , "0" , '1'
+#bool True, False
+#float 0.32, 1.23, 0.99
+x = 0
+name = 'Arsalan'
+print(x)
+print(name)
+name = "Traveler"
+print(name)
