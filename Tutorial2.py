a56b51afdc278e1abe1d4bdf761f3d2e286fa5e8
Arsalan <Arsalanbardsiri@gmail.com>
2021-01-29 17:19:58 -0800

    Tutorial #2

---
+++ Tutorial2.py
@@ -0,0 +1,27 @@
+#print("what is your name?")
+#name = input()
+#print('Hello,' , name)
+"""
+4 basic Operators
++
+-
+*
+/
+"""
+#num1 = 45
+#num2 = 3
+#print(num1 + num2)
+#print(num1 - num2)
+#print(num1 * num2)
+#print(num1 / num2)
+"""
+More Operators
+exponent **
+integer division //
+modules operator %
+"""
+#num3 = num1 ** num2
+#num4 = num1 // num2
+#num5 = num1 % num2
+#print(num3, num4, num5)
+
