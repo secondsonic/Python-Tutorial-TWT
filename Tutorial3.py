d4a1f931a2526ee36ec93616d28e0711d1fe4f16
Arsalan <Arsalanbardsiri@gmail.com>
2021-01-29 18:42:30 -0800

    Tutorial #3

---
+++ Tutorial3.py
@@ -0,0 +1,23 @@
+"""
+Comparison Operators 
+<
+>
+==
+!=
+"""
+#Conditions
+print(2 < 3)
+print(2 > 3)
+print(2 != 3)
+print(2 == 3)
+print('Hello' != "hellO")
+print('' == "")
+print("HELLO".lower() == 'hello')
+print()
+
+print(15 < 500)
+print(3-2+4 > 5)
+print(True != False)
+print(True == False)
+x = None #seems like None is same as null in java or php
+print(x)
