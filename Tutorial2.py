e95b71e7282067d33022d73d9ce23dc4363afed4
Arsalan <Arsalanbardsiri@gmail.com>
2021-01-29 17:30:40 -0800

    Tutorial #2

--- Tutorial2.py
+++ Tutorial2.py
@@ -25,3 +25,19 @@ modules operator %
 #num5 = num1 % num2
 #print(num3, num4, num5)
 
+print("Pick a number: ")
+num1 = input()
+print("Pick another number: ")
+num2 = input()
+
+SUM = num1 + num2
+print(type(SUM))
+print("This is and error: ", SUM) #give data type error since receives str not int
+
+SUM0 = int(num1) + int(num2)
+print(type(SUM0))
+print(SUM0)
+"""
+Also to convert to String
+str(3) = '3'
+"""
