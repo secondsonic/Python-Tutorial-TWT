a0ab7d8ed42f87e6f93900fca49c3a1efc4ad121
Arsalan <Arsalanbardsiri@gmail.com>
2021-01-29 20:17:15 -0800

    Tutorial #4

---
+++ Tutorial4.py
@@ -0,0 +1,37 @@
+"""
+if condition == true:
+    do this
+
+also 
+>= greater than or equal to
+<= less than or equal to
+"""
+"""
+age = int(input('Your Age: '))
+
+if age < 18:
+    print("You are younger than 18.")
+#OR
+
+age = input('Your Age: ')
+
+if int(age) > 18:
+    print("You are older than 18.")
+
+age = input('Your Age: ')
+
+if int(age) == 18:
+    print("You are 18.")
+
+age = input('Your Age: ')
+
+if int(age) != 18:
+    print("You are not 18.")
+"""
+age = input("Enter your age: ")
+if int(age) == 18:
+    print("You are 18.")
+elif int(age) < 18: #you can have as many as elif you want
+    print('You are younger than 18.')
+else:   #but only one else since it is a default
+    print("You are older than 18.")
