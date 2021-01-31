38b8aa2f7eed8737d5583ce1aaeb0232823dffd5
Arsalan <Arsalanbardsiri@gmail.com>
2021-01-30 18:48:26 -0800

    Modified Tutorial #6 and added Tutorial #7

---
+++ Tutorial6.py
@@ -0,0 +1,24 @@
+#For Loops
+
+"""
+for x in range(start,stop,step -> default = +1):
+    print(x)
+"""
+#1,2,3 will have the same out come
+for x in range(0,10,1):#1 start,stop and step
+    print(x)
+
+print()
+
+for x in range(0,10):#2 start and stop
+    print(x)
+
+print()
+
+for x in range(10):#3 only stop
+    print(x)
+
+print()
+
+for x in range(0,10,2):
+    print(x)
