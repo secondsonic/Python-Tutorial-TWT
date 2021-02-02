5642281c11a81a2ffdd5571a514d991b97067d69
Arsalan <Arsalanbardsiri@gmail.com>
2021-02-01 17:59:08 -0800

    Turorial #7

--- Tutorial7.py
+++ Tutorial7.py
@@ -1,1 +1,26 @@
 #While Loops
+"""
+while condition == True:
+    do this
+
+Goes on till condition == False or we can use 'break'
+"""
+x = 0
+
+while x < 10:
+    x = x + 1
+    print(x)
+
+loop = True
+
+while loop:
+    name = input('Enter some words1: ')
+    if name == 'stop':
+        loop = False
+#OR
+y = True
+
+while y:
+    name = input('Enter some words2: ')
+    if name == 'stop':
+        break
